b742667c4936b875974c90b6c1098caf9bfc70af
Madhav Shenoy <mshenoy83@gmail.com>
2025-08-05 10:57:54 +1000

    Exports Terraform outputs to GitHub Actions
    Adds functionality to export Terraform outputs as GitHub Actions outputs.
    This allows subsequent steps in the workflow to access and use the values
    produced by Terraform.

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,9 +24,11 @@ infraterraformDir="$6"
 cd "$infraterraformDir" || exit
 
 # Initialize terraform with the provided backend configuration
-terraform init -backend-config="bucket=$backendBucket" -backend-config="region=$regionName" \
--backend-config="profile=$profileName" -backend-config="dynamodb_table=$backendDynamoDB" \
--backend=true -force-copy -get=true -input=false
+terraform init -backend-config="bucket=$backendBucket" \
+               -backend-config="region=$regionName" \
+               -backend-config="profile=$profileName" \
+               -backend-config="dynamodb_table=$backendDynamoDB" \
+               -backend=true -force-copy -get=true -input=false
 
 # Validate the terraform configuration
 terraform validate
@@ -36,3 +38,19 @@ terraform plan -out=tfplan
 
 # Apply the saved plan
 terraform apply tfplan
+
+# Export Terraform outputs to GitHub Actions outputs
+echo "Exporting Terraform outputs..."
+output_json=$(terraform output -json)
+
+if [[ "$output_json" != "{}" ]]; then
+  echo "$output_json" | jq -r 'to_entries[] | "\(.key)<<EOF\n\(.value.value)\nEOF"' | while IFS= read -r line; do
+    if [[ $line == *"<<EOF" ]]; then
+      key=$(echo "$line" | cut -d'<' -f1)
+      read -r value
+      echo "$key=$value" >> "$GITHUB_OUTPUT"
+    fi
+  done
+else
+  echo "No Terraform outputs found."
+fi
